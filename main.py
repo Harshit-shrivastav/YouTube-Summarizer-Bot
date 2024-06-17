584b5b7654af436ae97a8bcd163541bac64fd71f
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-06-17 17:58:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,9 @@ async def get_groq_response(user_prompt, system_prompt):
 @client.on(events.NewMessage(pattern='/start'))
 async def start(event):
     await event.reply('Send me a YouTube link, and I will summarize that video for you in text format.')
-
+    if not await db.is_inserted("users", event.sender_id):
+        await db.insert("users", event.sender_id)
+        
 @client.on(events.NewMessage)
 async def handle_message(event):
     url = event.message.message
