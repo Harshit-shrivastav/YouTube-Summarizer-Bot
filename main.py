c10dc52cceb15a17fe75752a264f3bd625d95fc1
Harshit Shrivastav <harshitshrivastav@hotmail.com>
2024-06-18 13:26:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -72,7 +72,12 @@ async def start(event):
     await event.reply('Send me a YouTube link, and I will summarize that video for you in text format.')
     if not await db.is_inserted("users", event.sender_id):
         await db.insert("users", event.sender_id)
-        
+
+@client.on(events.NewMessage(pattern='/users'))
+async def start(event):
+    users = len(await db.fetch_all("users"))
+    await event.reply(f'Total Users: {users}')
+
 @client.on(events.NewMessage)
 async def handle_message(event):
     url = event.message.message
@@ -152,6 +157,31 @@ async def handle_message(event):
         print("Invalid YouTube link.")
         await event.reply('Please send a valid YouTube link.')
 
+@client.on(events.NewMessage(pattern='/bcast'))
+async def start(event):
+    if not event.reply_to_msg_id:
+        return await event.reply(
+            "Please use `/bcast` as reply to the message you want to broadcast."
+        )
+    msg = await event.get_reply_message()
+    xx = await event.reply("In progress...")
+    users = await db.fetch_all('users')
+    done = error = 0
+    for user_id in users:
+        try:
+            await TelegramBot.send_message(
+                int(user_id),
+                msg.text.format(user=(await TelegramBot.get_entity(int(user_id))).first_name),
+                file=msg.media,
+                buttons=msg.buttons,
+                link_preview=False,
+            )
+            done += 1
+        except Exception as brd_er:
+            log.error("Broadcast error:\nChat: %d\nError: %s", int(user_id), brd_er)
+            error += 1
+    await xx.edit(f"Broadcast completed.\nSuccess: {done}\nFailed: {error}")
+    
 async def main():
     await client.start(bot_token=BOT_TOKEN)
     print("Bot is running...\nHit 🌟 on github repo if you liked my work and please follow on github for more such repos.")
