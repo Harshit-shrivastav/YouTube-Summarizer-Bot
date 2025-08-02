edbea622e0be6ee8b57ef0e363acf7650636e1f3
Harshit Shrivastav <92732265+Harshit-shrivastav@users.noreply.github.com>
2025-08-02 11:06:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Telegram:
-    API_ID = int(os.environ.get('API_ID'))
-    API_HASH = os.environ.get('API_HASH')
     BOT_TOKEN = os.environ.get('BOT_TOKEN')
     AUTH_USER_ID = int(os.environ.get('AUTH_USER_ID'))
     
 class Ai:
     API_KEY = os.environ.get('AI_API_KEY')
-    MODEL_NAME = os.environ.get('AI_MODEL_NAME', 'gemini-2.5-flash)
+    MODEL_NAME = os.environ.get('AI_MODEL_NAME', 'gemini-2.5-flash')
     API_URL = os.environ.get('AI_API_URL', 'https://generativelanguage.googleapis.com/v1beta/openai/')
     
 class Database:
